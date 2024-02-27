0993b09067af5cea9f5205faa77a0aaed6855c28
Daniel Bershtein <danbrs1993@gmail.com>
2024-02-27 02:42:02 +0200

    table

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,24 +1,30 @@
 import { students } from "../data/students";
 function Dashboard(props) {
   return (
-    <table>
-      <thead>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Major</th>
-        <th>University</th>
-        <th>Average Grade</th>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{props.name}</td>
-          <td>{props.age}</td>
-          <td>{props.major}</td>
-          <td>{props.university}</td>
-          <td>{props.averageGrade}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="student-list">
+      {props.students.map((student) => (
+        <div className="student" key={student.id}>
+          <table>
+            <thead>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Major</th>
+              <th>University</th>
+              <th>Average Grade</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{student.name}</td>
+                <td>{student.age}</td>
+                <td>{student.major}</td>
+                <td>{student.university}</td>
+                <td>{student.averageGrade}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      ))}
+    </div>
   );
 }
 
